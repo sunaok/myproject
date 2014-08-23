3085a598a486c34b6e31d22ac56d163810fef7c1
sunao <konkonkonkons@sunao-no-MacBook-Pro.local>
2014-08-23 15:28:21 +0900

    Initial commit

---
+++ src/main/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	f "fmt"
+	//"github.com/sunaok/gosample"
+)
+
+func sum(i, j int) int {
+	return i + j
+}
+func swap(i, j int) (int, int) {
+	return j, i
+}
+
+func main() {
+	//	f.Println(sum(1,2))
+	x, y := 1, 2
+	x, y = swap(x,y)
+	f.Println(x,y)
+	//f.Print(gosample.Message)
+	//a,b := 110,100
+	//	if a > b {
+	//		f.Print("a is larger than b")
+	//	}else if a < b {
+	//		f.Print("a is smaller than b")
+	//	}else{
+	//		f.Print("a equals b")
+	////	}
+	//	for i := 0;i < 10 ; i++{
+	//		f.Println(i)
+	//	}
+	//	n := 0
+	//	for n < 100{
+	//		f.Println(n)
+	//		n++
+	//	}
+	//
+	//	n:=0
+	//	for n<100{
+	//	switch n{
+	//	case 15:
+	//		f.Println("Fizzbuzz")
+	//	case 5,10:
+	//		f.Println("Buzz")
+	//	case 3,6,9:
+	//		f.Println("Fizz")
+	//	default:
+	//		f.Println(n)
+	//	}
+	//		n++
+	//	}
+
+}
